7fc57765b69d20f6d47bf2bcc047a7a821f15a32
im7mortal <im7mortal@gmail.com>
2017-02-23 10:50:27 -0800

    adjust main It doesn't work like expected

--- test/exe/main.go
+++ test/exe/main.go
@@ -4,11 +4,19 @@ import (
 	"flag"
 	"github.com/golang/glog"
 	"github.com/sturfeeinc/cglog/test"
+	"time"
 )
 
-func main() {
+func init() {
 	flag.Parse()
+	//flag.Lookup("alsologtostderr").Value.Set("true")
+}
+
+func main() {
+	time.Sleep(2 * time.Second)
 	glog.Info("Started from go")
 	cglogtest.Test()
 	glog.Info("Finished from go")
+	glog.Info("Finished from go")
+	time.Sleep(2 * time.Second)
 }
